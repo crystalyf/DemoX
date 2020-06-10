45369e7b63ce2bef195a5a7ead59a3794eb59afb
xingjunchao <xingjunchao@fenrir-tec.com>
2020-06-10 14:49:35 +0800

    只保留2种主题色的切换

--- app/src/main/java/com/change/demox/themecolor/screen/Colorful.java
+++ app/src/main/java/com/change/demox/themecolor/screen/Colorful.java
@@ -89,89 +89,13 @@ public class Colorful {
 
     public enum ThemeColor {
         /**
-         * RED
-         */
-        RED(R.color.md_red_500, R.color.md_red_700),
-        /**
-         * PINK
-         */
-        PINK(R.color.md_pink_500, R.color.md_pink_700),
-        /**
-         * PURPLE
-         */
-        PURPLE(R.color.md_purple_500, R.color.md_purple_700),
-        /**
-         * DEEP_PURPLE
-         */
-        DEEP_PURPLE(R.color.md_deep_purple_500, R.color.md_deep_purple_700),
-        /**
-         * INDIGO
-         */
-        INDIGO(R.color.md_indigo_500, R.color.md_indigo_700),
-        /**
          * BLUE
          */
         BLUE(R.color.md_blue_500, R.color.md_blue_700),
         /**
-         * LIGHT_BLUE
-         */
-        LIGHT_BLUE(R.color.md_light_blue_500, R.color.md_light_blue_700),
-        /**
-         * CYAN
-         */
-        CYAN(R.color.md_cyan_500, R.color.md_cyan_700),
-        /**
-         * TEAL
-         */
-        TEAL(R.color.md_teal_500, R.color.md_teal_700),
-        /**
          * GREEN
          */
-        GREEN(R.color.md_green_500, R.color.md_green_700),
-        /**
-         * LIGHT_GREEN
-         */
-        LIGHT_GREEN(R.color.md_light_green_500, R.color.md_light_green_700),
-        /**
-         * LIME
-         */
-        LIME(R.color.md_lime_500, R.color.md_lime_700),
-        /**
-         * YELLOW
-         */
-        YELLOW(R.color.md_yellow_500, R.color.md_yellow_700),
-        /**
-         * AMBER
-         */
-        AMBER(R.color.md_amber_500, R.color.md_amber_700),
-        /**
-         * ORANGE
-         */
-        ORANGE(R.color.md_orange_500, R.color.md_orange_700),
-        /**
-         * DEEP_ORANGE
-         */
-        DEEP_ORANGE(R.color.md_deep_orange_500, R.color.md_deep_orange_700),
-        /**
-         * BROWN
-         */
-        BROWN(R.color.md_brown_500, R.color.md_brown_700),
-        /**
-         * GREY
-         */
-        GREY(R.color.md_grey_500, R.color.md_grey_700),
-        /**
-         * BLUE_GREY
-         */
-        BLUE_GREY(R.color.md_blue_grey_500, R.color.md_blue_grey_700),
-        /**
-         * WHITE
-         */
-        WHITE(R.color.md_white_1000, R.color.md_white_1000),
-        /**
-         * BLACK
-         */
-        BLACK(R.color.md_black_1000, R.color.md_black_1000);
+        GREEN(R.color.md_green_500, R.color.md_green_700);
         @ColorRes
         private int colorRes;
         @ColorRes
@@ -194,8 +118,8 @@ public class Colorful {
     }
 
     public static class Defaults {
-        private static ThemeColor primaryColor = ThemeColor.DEEP_PURPLE;
-        private static ThemeColor accentColor = ThemeColor.RED;
+        private static ThemeColor primaryColor = ThemeColor.BLUE;
+        private static ThemeColor accentColor = ThemeColor.BLUE;
         private static boolean trans = false;
         private static boolean darkTheme = false;
 
