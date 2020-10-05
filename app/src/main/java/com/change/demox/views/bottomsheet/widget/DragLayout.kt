0e20568561d5f5723ea9b5c0d281d0cf87b42565
xingjunchao <xingjunchao@fenrir-tec.com>
2020-10-05 11:27:27 +0800

    View： 补充bottomSheet效果的下拉注释

--- app/src/main/java/com/change/demox/views/bottomsheet/widget/DragLayout.kt
+++ app/src/main/java/com/change/demox/views/bottomsheet/widget/DragLayout.kt
@@ -1,10 +1,3 @@
-/*
- * DragLayout.kt
- *
- * Created by xingjunchao on 2020/06/17.
- * Copyright © 2020年 Kubota-PAD. All rights reserved.
- */
-
 package com.change.demox.views.bottomsheet.widget
 
 import android.annotation.SuppressLint
@@ -15,7 +8,6 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.core.view.ViewCompat
 import androidx.customview.widget.ViewDragHelper
@@ -30,15 +22,20 @@ class DragLayout : RelativeLayout {
 
     /**
      * ViewDragHelper是针对 ViewGroup 中的拖拽和重新定位 views 操作时提供了一系列非常有用的方法和状态追踪。基本上使用在自定义ViewGroup处理拖拽中
+     *
+     *  Android布局中要对布局中的控件进行自由拖动,一种方法是重写父类点击事件的方法, 对触摸事件进行处理, 这种方法代码量过大暂不讨论.
+     *  另一种方法是利用ViewDragHelper来处理触摸事件
      */
-
     private var dragHelper: ViewDragHelper? = null
-    private var dragView: View? = null
+
+    //拖拽布局的横条
+    private var dragViewLayout: View? = null
+
+    //拖拽显示出来的列表父布局
     private var contentView: FrameLayout? = null
-    private var progressLayout: FrameLayout? = null
+
+    //拖拽显示出来的列表
     private var recyclerView: RecyclerView? = null
-    private var netWorkErrorLayout: LinearLayout? = null
-    private var maintenanceLayout: LinearLayout? = null
     private var dragRange = 0
     private var topMargin = 0
     var currentState = State.AT_BOTTOM
@@ -79,19 +76,83 @@ class DragLayout : RelativeLayout {
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        dragView = findViewById(R.id.dragView)
+        dragViewLayout = findViewById(R.id.dragView)
         contentView = findViewById(R.id.frame_list)
         recyclerView = findViewById(R.id.rv_machine_part)
     }
+
+    /**
+     * 处理CallBack逻辑：
+     *
+    ​CallBack中判断控件是否可以拖动有以下几个关键方法:
+
+    tryCaptureView() : 判断View是否是可拖动, 返回true表示可该view可拖动
+
+    clampViewPositionHorizontal() / clampViewPositionVertical() : 决定子view在水平/垂直方向上应该移动到的位置, 返回0表示不允许该方向上的运动
+
+    getViewHorizontalDragRange() / getViewVerticalDragRange() : 以像素为单位返回子view在水平/垂直方向上可移动的距离, 返回0表示不能在该方向上进行移动
+
+    onViewPositionChanged()会在控件位置变化时不断被回调
+
+    onViewReleased()则是在手指松开时进行回调
+
+     */
 
     private val callback: ViewDragHelper.Callback = object : ViewDragHelper.Callback() {
         override fun tryCaptureView(
                 child: View,
                 pointerId: Int
         ): Boolean {
-            return child === dragView
+            //若控件是dragView,那么可拖动
+            return child === dragViewLayout
         }
 
+        /**
+         * 限制View纵向的拖拉操作
+         *
+         * 返回0表示不允许该方向上的运动
+         */
+        override fun clampViewPositionVertical(
+                child: View,
+                top: Int,
+                dy: Int
+        ): Int {
+            //滑动限制距离，控制view纵向不超出屏幕
+            val topBound = height - dragRange - dragViewLayout!!.height
+            val bottomBound = height - dragViewLayout!!.height
+            topMargin = top
+            return topBound.coerceAtLeast(top).coerceAtMost(bottomBound)
+        }
+
+        override fun getViewVerticalDragRange(child: View): Int {
+            return dragRange
+        }
+
+
+        /**
+         * 重写, 对自定义布局进行一些处理
+         */
+        override fun onViewReleased(
+                releasedChild: View,
+                xVel: Float,
+                yVel: Float
+        ) {
+            super.onViewReleased(releasedChild, xVel, yVel)
+            //根据上边距，判断滑动显示的bottomSheet是显示在屏幕中的什么位置:top,middle,bottom
+            if (topMargin < dragRange / 4) {
+                smoothToTop()
+            } else if (topMargin > dragRange / 4 && topMargin < 3 * dragRange / 4) {
+                smoothToMid()
+            } else if (topMargin > 3 * dragRange / 4) {
+                smoothToBottom()
+            }
+        }
+
+        /**
+         * 重写, 对自定义布局进行一些处理
+         *
+         * 记录控件所在的位置, 然后ViewGroup的onLayout()方法并指定其位置就可以了
+         */
         override fun onViewPositionChanged(
                 changedView: View,
                 left: Int,
@@ -99,15 +160,15 @@ class DragLayout : RelativeLayout {
                 dx: Int,
                 dy: Int
         ) {
-            dragView!!.layout(
-                    width - dragView!!.width,
+            dragViewLayout!!.layout(
+                    width - dragViewLayout!!.width,
                     top,
                     width,
-                    top + dragView!!.height
+                    top + dragViewLayout!!.height
             )
             contentView!!.layout(
                     width - contentView!!.width,
-                    top + dragView!!.height - 2,
+                    top + dragViewLayout!!.height - 2,
                     width,
                     height
             )
@@ -118,38 +179,14 @@ class DragLayout : RelativeLayout {
                     dragRange - top
             )
         }
-
-        override fun clampViewPositionVertical(
-                child: View,
-                top: Int,
-                dy: Int
-        ): Int {
-            val topBound = height - dragRange - dragView!!.height
-            val bottomBound = height - dragView!!.height
-            topMargin = top
-            return topBound.coerceAtLeast(top).coerceAtMost(bottomBound)
-        }
-
-        override fun getViewVerticalDragRange(child: View): Int {
-            return dragRange
-        }
-
-        override fun onViewReleased(
-                releasedChild: View,
-                xVel: Float,
-                yVel: Float
-        ) {
-            super.onViewReleased(releasedChild, xVel, yVel)
-            if (topMargin < dragRange / 4) {
-                smoothToTop()
-            } else if (topMargin > dragRange / 4 && topMargin < 3 * dragRange / 4) {
-                smoothToMid()
-            } else if (topMargin > 3 * dragRange / 4) {
-                smoothToBottom()
-            }
-        }
     }
 
+    /**
+     * 测量控件的高度，可以得到每个控件的最终高度
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         dragRange = contentView!!.measuredHeight
@@ -166,68 +203,12 @@ class DragLayout : RelativeLayout {
         reLayout(currentState)
     }
 
-    fun reLayout(currentState: Int) {
-        when (currentState) {
-            State.AT_BOTTOM -> {
-                dragView!!.layout(
-                        width - dragView!!.width,
-                        height - dragView!!.height,
-                        width,
-                        height
-                )
-                contentView!!.layout(width - dragView!!.width, height, width, height)
-            }
-            State.AT_MIDDLE -> {
-                dragView!!.layout(
-                        width - dragView!!.width,
-                        (height - dragView!!.height) / 2,
-                        width,
-                        (height + dragView!!.height) / 2
-                )
-                contentView!!.layout(
-                        width - dragView!!.width,
-                        (height + dragView!!.height) / 2 - 2,
-                        width,
-                        height
-                )
-                recyclerView!!.layout(
-                        0,
-                        0,
-                        contentView!!.width,
-                        contentView!!.height
-                )
-                recyclerView!!.layoutParams.height = (height - dragView!!.height) / 2
-            }
-            State.AT_TOP -> {
-                dragView!!.layout(
-                        width - dragView!!.width,
-                        height - dragRange - dragView!!.height,
-                        width,
-                        height - dragRange
-                )
-                contentView!!.layout(
-                        width - dragView!!.width,
-                        dragView!!.height - 2,
-                        width,
-                        height
-                )
-                recyclerView!!.layout(
-                        0,
-                        0,
-                        contentView!!.width,
-                        contentView!!.height
-                )
-                recyclerView!!.layoutParams.height = height - dragView!!.height
-            }
-        }
-    }
-
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         val pointY = ev?.y
         val pointX = ev?.x
 
         if (pointY != null && pointX != null) {
-            return if (pointY >= dragView?.top!! && pointY <= dragView!!.bottom && pointX >= dragView!!.left && pointX <= dragView!!.right) {
+            return if (pointY >= dragViewLayout?.top!! && pointY <= dragViewLayout!!.bottom && pointX >= dragViewLayout!!.left && pointX <= dragViewLayout!!.right) {
                 if (ev.action == MotionEvent.ACTION_DOWN) {
                     onTouchEvent(ev)
                 } else {
@@ -239,48 +220,16 @@ class DragLayout : RelativeLayout {
         return super.dispatchTouchEvent(ev)
     }
 
+    /**
+     * 写拖拽的同时，一般要重写onTouchEvent()方法, 使ViewDragHelper接管触摸事件的处理
+     *
+     */
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        //通过这个方法判断是否处理拦截的触摸事件，这里使ViewDragHelper接管触摸事件的处理
         dragHelper!!.processTouchEvent(event)
         return true
-    }
-
-    private fun smoothToTop() {
-        currentState = State.AT_TOP
-        recyclerView!!.layoutParams.height = height - dragView!!.height
-        if (dragHelper!!.smoothSlideViewTo(
-                        dragView!!,
-                        width - dragView!!.width,
-                        height - dragRange - dragView!!.height
-                )
-        ) {
-            ViewCompat.postInvalidateOnAnimation(this)
-        }
-    }
-
-    fun smoothToMid() {
-        currentState = State.AT_MIDDLE
-        recyclerView!!.layoutParams.height = (height - dragView!!.height) / 2
-        if (dragHelper!!.smoothSlideViewTo(
-                        dragView!!,
-                        width - dragView!!.width,
-                        (height - dragView!!.height) / 2
-                )
-        ) {
-            ViewCompat.postInvalidateOnAnimation(this)
-        }
-    }
-
-    private fun smoothToBottom() {
-        currentState = State.AT_BOTTOM
-        if (dragHelper!!.smoothSlideViewTo(
-                        dragView!!,
-                        width - dragView!!.width,
-                        height - dragView!!.height
-                )
-        ) {
-            ViewCompat.postInvalidateOnAnimation(this)
-        }
     }
 
     override fun computeScroll() {
@@ -305,5 +254,99 @@ class DragLayout : RelativeLayout {
             reLayout(currentState)
         }
         super.onRestoreInstanceState(mState)
+    }
+
+    private fun reLayout(currentState: Int) {
+        when (currentState) {
+            State.AT_BOTTOM -> {
+                dragViewLayout!!.layout(
+                        width - dragViewLayout!!.width,
+                        height - dragViewLayout!!.height,
+                        width,
+                        height
+                )
+                contentView!!.layout(width - dragViewLayout!!.width, height, width, height)
+            }
+            State.AT_MIDDLE -> {
+                dragViewLayout!!.layout(
+                        width - dragViewLayout!!.width,
+                        (height - dragViewLayout!!.height) / 2,
+                        width,
+                        (height + dragViewLayout!!.height) / 2
+                )
+                contentView!!.layout(
+                        width - dragViewLayout!!.width,
+                        (height + dragViewLayout!!.height) / 2 - 2,
+                        width,
+                        height
+                )
+                recyclerView!!.layout(
+                        0,
+                        0,
+                        contentView!!.width,
+                        contentView!!.height
+                )
+                recyclerView!!.layoutParams.height = (height - dragViewLayout!!.height) / 2
+            }
+            State.AT_TOP -> {
+                dragViewLayout!!.layout(
+                        width - dragViewLayout!!.width,
+                        height - dragRange - dragViewLayout!!.height,
+                        width,
+                        height - dragRange
+                )
+                contentView!!.layout(
+                        width - dragViewLayout!!.width,
+                        dragViewLayout!!.height - 2,
+                        width,
+                        height
+                )
+                recyclerView!!.layout(
+                        0,
+                        0,
+                        contentView!!.width,
+                        contentView!!.height
+                )
+                recyclerView!!.layoutParams.height = height - dragViewLayout!!.height
+            }
+        }
+    }
+
+    private fun smoothToTop() {
+        currentState = State.AT_TOP
+        recyclerView!!.layoutParams.height = height - dragViewLayout!!.height
+        if (dragHelper!!.smoothSlideViewTo(
+                        dragViewLayout!!,
+                        width - dragViewLayout!!.width,
+                        height - dragRange - dragViewLayout!!.height
+                )
+        ) {
+            ViewCompat.postInvalidateOnAnimation(this)
+        }
+    }
+
+    private fun smoothToMid() {
+        currentState = State.AT_MIDDLE
+        recyclerView!!.layoutParams.height = (height - dragViewLayout!!.height) / 2
+        if (dragHelper!!.smoothSlideViewTo(
+                        dragViewLayout!!,
+                        width - dragViewLayout!!.width,
+                        (height - dragViewLayout!!.height) / 2
+                )
+        ) {
+            ViewCompat.postInvalidateOnAnimation(this)
+        }
+    }
+
+    private fun smoothToBottom() {
+        currentState = State.AT_BOTTOM
+        if (dragHelper!!.smoothSlideViewTo(
+                        dragViewLayout!!,
+                        width - dragViewLayout!!.width,
+                        height - dragViewLayout!!.height
+                )
+        ) {
+            ViewCompat.postInvalidateOnAnimation(this)
+        }
     }
 }
