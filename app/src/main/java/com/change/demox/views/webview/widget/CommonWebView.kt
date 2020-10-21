aa5d4f6bdbe99dfca3223d3bf4f99ecd9bcfab51
xingjunchao <xingjunchao@fenrir-tec.com>
2020-10-21 11:07:15 +0800

    View： WebView：注释补充

--- app/src/main/java/com/change/demox/views/webview/widget/CommonWebView.kt
+++ app/src/main/java/com/change/demox/views/webview/widget/CommonWebView.kt
@@ -139,6 +139,14 @@ class CommonWebView @JvmOverloads constructor(
 
     internal inner class ChromeClient : WebChromeClient() {
 
+        /**
+         * 根据这个回调，可以控制进度条的进度（包括显示与隐藏）。一般情况下，想要达到100%的进度需要的时间较长（特别是首次加载），
+         * 用户长时间等待进度条不消失必定会感到焦虑，影响体验。其实当progress达到80的时候，加载出来的页面已经基本可用了。事实上，
+         * 国内厂商大部分都会提前隐藏进度条，让用户以为网页加载很快
+         *
+         * @param view
+         * @param newProgress
+         */
         override fun onProgressChanged(view: WebView?, newProgress: Int) {
             if (mListener != null) {
                 mListener?.onProgress(newProgress)
@@ -146,6 +154,13 @@ class CommonWebView @JvmOverloads constructor(
             super.onProgressChanged(view, newProgress)
         }
 
+        /**
+         * 用来设置标题。需要注意的是，在部分Android系统版本中可能会回调多次这个方法，而且有时候回调的title是一个url，
+         * 客户端可以针对这种情况进行特殊处理，避免在标题栏显示不必要的链接
+         *
+         * @param view
+         * @param title
+         */
         override fun onReceivedTitle(view: WebView?, title: String?) {
             super.onReceivedTitle(view, title)
             if (mListener != null) {
@@ -164,6 +179,12 @@ class CommonWebView @JvmOverloads constructor(
             }
         }
 
+        /**
+         * 加载页面结束回调
+         *
+         * @param view
+         * @param url
+         */
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
             if (!Utils.hasNetWork(context)) {
@@ -176,6 +197,13 @@ class CommonWebView @JvmOverloads constructor(
             }
         }
 
+        /**
+         * 如果遇到了重定向，或者点击了页面中的a标签实现页面跳转，那么会回调这个方法。可以说这个是WebView里面最重要的回调之一
+         *
+         * @param view
+         * @param url
+         * @return
+         */
         override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
             //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
             return if (Utils.isUrlNeedShowInBrowser(url)) {
@@ -196,6 +224,15 @@ class CommonWebView @JvmOverloads constructor(
             }
         }
 
+        /**
+         * 加载页面的过程中发生了错误，会回调这个方法。主要是http错误以及ssl错误。在这两个回调中，我们可以进行异常上报，监控异常页面、过期页面，及时反馈给运营或前端修改。
+         * 在处理ssl错误时，遇到不信任的证书可以进行特殊处理，例如对域名进行判断，针对自己公司的域名“放行”，防止进入丑陋的错误证书页面。也可以与Chrome一样，
+         * 弹出ssl证书疑问弹窗，给用户选择的余地
+         *
+         * @param view
+         * @param request
+         * @param error
+         */
         override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
             super.onReceivedError(view, request, error)
             if (request?.isForMainFrame != false) {
