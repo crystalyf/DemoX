719928e45265b099881c1db605b3ef3053eff82a
xingjunchao <xingjunchao@fenrir-tec.com>
2021-03-22 18:03:03 +0800

    测试git

--- app/src/main/java/com/change/demox/views/webview/WebViewActivity.kt
+++ app/src/main/java/com/change/demox/views/webview/WebViewActivity.kt
@@ -12,6 +12,8 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+
         initView()
     }
 
