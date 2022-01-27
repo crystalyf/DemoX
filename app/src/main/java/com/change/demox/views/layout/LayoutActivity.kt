48b87428d586dd31377dac07d9ddd4c389a61e6c
xingjunchao <xingjunchao@fenrir-tec.com>
2022-01-27 14:23:23 +0800

    Switch开关

--- app/src/main/java/com/change/demox/views/layout/LayoutActivity.kt
+++ app/src/main/java/com/change/demox/views/layout/LayoutActivity.kt
@@ -2,7 +2,10 @@ package com.change.demox.views.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.Toast
 import com.change.demox.R
+import kotlinx.android.synthetic.main.activity_layout.*
 
 class LayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,14 @@ class LayoutActivity : AppCompatActivity() {
     }
 
     private fun initView(){
-
+        sw_state.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                //true
+                Toast.makeText(this,isChecked.toString(),Toast.LENGTH_SHORT).show()
+            } else {
+                //false
+                Toast.makeText(this,isChecked.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
